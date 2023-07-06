352224ae00ed3e839a7d52e6c86f5eefea62b72e
bool <zhenbeigg@163.com>
2023-07-06 10:30:03 +0800

    删除：日志函数

--- src/Functions.php
+++ src/Functions.php
@@ -20,7 +20,6 @@ use Swoole\Websocket\Frame;
 use Swoole\WebSocket\Server as WebSocketServer;
 use App\Constants\ErrorCode;
 use App\Exception\EyCException;
-use Eykj\AliyunSls\ClientInterface;
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Context\ApplicationContext;
 use function Hyperf\Support\env;
@@ -493,51 +492,6 @@ if (!function_exists('timech')) {
             $time = strtotime($f[1] . $tp, $time);
         }
         return $type ? date($type, $time) : $time;
-    }
-}
-
-if (!function_exists('alog')) {
-    /**
-     * @brief 阿里云日志
-     * @param array $data 日志内容
-     * @param int $type 类型 0 INFO:打印输出 1 WARRING:警告 2 ERROR:错误日志 3 CALLBACK:回调日志  4 SQL:数据库日志 5 SOCKET:socket日志 
-     */
-    function alog($data, int $type = 0): void
-    {
-        switch ($type) {
-            case '0':
-                $k = 'INFO::';
-                break;
-            case '1':
-                $k = 'WARRING::';
-                break;
-            case '2':
-                $k = 'ERROR::';
-                break;
-            case '3':
-                $k = 'CALLBACK::';
-                break;
-            case '4':
-                $k = 'SQL::';
-                break;
-            case '5':
-                $k = 'SOCKET::';
-                break;
-            case '6':
-                $k = 'MQTT::';
-                break;
-
-            default:
-                $k = 'SQL::';
-                break;
-        }
-        /* 添加阿里云sls日志 */
-        if (is_array($data) || is_object($data)) {
-            $data = json_encode($data, 320);
-        }
-        if (env('ALIYUN_SLS_ENDPOINT')) {
-            container()->get(ClientInterface::class)->putLogs([$k => $data]);
-        }
     }
 }
 
