d9ea81bf8c79b89fc64ea2228d8594022d24a075
bool <zhenbeigg@163.com>
2023-07-20 11:02:45 +0800

    修改:审批服务接口类

--- src/JsonRpcInterface/ApproveInterface.php
+++ src/JsonRpcInterface/ApproveInterface.php
@@ -32,7 +32,12 @@ class ApproveInterface
      */
     public function post_add(string $type, array $param, array $val = [])
     {
-        return $this->Service->post_add($type, $param, $val);
+        try {
+            return $this->Service->post_add($type, $param, $val);
+        } catch (\Exception $e) {
+            alog($e->getMessage(), 2);
+            return false;
+        }
     }
     /**
      * @author: 布尔
@@ -43,7 +48,12 @@ class ApproveInterface
      */
     public function post_modify(string $type, array $param, array $val = []): int
     {
-        return $this->Service->post_modify($type, $param, $val);
+        try {
+            return $this->Service->post_modify($type, $param, $val);
+        } catch (\Exception $e) {
+            alog($e->getMessage(), 2);
+            return false;
+        }
     }
     /**
      * @author: 布尔
@@ -70,7 +80,13 @@ class ApproveInterface
      */
     public function get_ls(string $type, array $param, array $key = [], int $per_page = 10, array $order = [], ?int $page = 0): array
     {
-        return $this->Service->get_ls($type, $param, $key, $per_page, $order, $page);
+        try {
+            return $this->Service->get_ls($type, $param, $key, $per_page, $order, $page);
+        } catch (\Exception $e) {
+            alog($e->getMessage(), 2);
+            return [];
+        }
+
     }
     /**
      * @author: 布尔
@@ -81,6 +97,11 @@ class ApproveInterface
      */
     public function get_all(string $type, array $param, array $key = []): array
     {
-        return $this->Service->get_all($type, $param, $key);
+        try {
+            return $this->Service->get_all($type, $param, $key);
+        } catch (\Exception $e) {
+            alog($e->getMessage(), 2);
+            return [];
+        }
     }
 }
