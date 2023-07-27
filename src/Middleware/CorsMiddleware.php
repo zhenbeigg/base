d628c8601df84a09aa59ca9a0df91fec74c01b57
bool <zhenbeigg@163.com>
2023-07-27 20:27:30 +0800

    修改：跨域中间件

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -3,7 +3,7 @@
  * @author: 布尔
  * @name: 类名
  * @desc: 介绍
- * @LastEditTime: 2022-11-04 20:24:59
+ * @LastEditTime: 2023-07-27 20:26:48
  */
 
 declare(strict_types=1);
@@ -29,7 +29,7 @@ class CorsMiddleware implements MiddlewareInterface
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
             // Headers 可以根据实际情况进行改写。
-            ->withHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization,content-type,accept,Access-Control-Allow-Origin,responseType');
+            ->withHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization,content-type,accept,Access-Control-Allow-Origin,responseType,satoken');
 
         Context::set(ResponseInterface::class, $response);
 
