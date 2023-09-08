dd134b729c9f25fc8cd4d82b276ff6416f4636bd
bool <bool@eykj.cn>
2023-09-08 16:31:11 +0800

    修改：就餐接口接口类

--- src/JsonRpcInterface/CanyinInterface.php
+++ src/JsonRpcInterface/CanyinInterface.php
@@ -175,14 +175,14 @@ class CanyinInterface
 
     /**
      * @author: 布尔
-     * @name: 支付
+     * @name: 三方支付-发起支付
      * @param {array} $param
      * @return {array}
      */
-    public function post_pay(array $param)
+    public function post_pay_scanpay(array $param)
     {
         try {
-            return $this->Service->post_pay($param);
+            return $this->Service->post_pay_scanpay($param);
         } catch (\Exception $e) {
             error($e->getCode(), $e->getMessage());
         }
@@ -190,14 +190,14 @@ class CanyinInterface
 
     /**
      * @author: 布尔
-     * @name: 查询支付订单
+     * @name: 三方支付-查询
      * @param {array} $param
      * @return {array}
      */
-    public function get_pay_info(array $param)
+    public function get_pay_query(array $param)
     {
         try {
-            return $this->Service->get_pay_info($param);
+            return $this->Service->get_pay_query($param);
         } catch (\Exception $e) {
             error($e->getCode(), $e->getMessage());
         }
