70cfe9c5bdb540d922042baceb27beb6ee91ac74
bool <zhenbeigg@163.com>
2023-09-13 11:55:16 +0800

    修改：设备中心接口类

--- src/JsonRpcInterface/DeviceInterface.php
+++ src/JsonRpcInterface/DeviceInterface.php
@@ -220,4 +220,19 @@ class DeviceInterface
     {
         return $this->Service->post_face_modify_send($param);
     }
+    /**
+     * @author: 布尔
+     * @name: 获取token
+     * @param {array} $param
+     * @param {string} $type 对应service类  Dtalk:钉钉 Modian:魔点
+     * @return {array}
+     */
+    public function get_access_token(string $type, array $param)
+    {
+        try {
+            return $this->Service->get_access_token($type, $param);
+        } catch (\Exception $e) {
+            error($e->getCode(), $e->getMessage());
+        }
+    }
 }
